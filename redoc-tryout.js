c03f9472b87b00393daa3500ddda93f45dbbaeac
YusorQi <youser.rasheed@qi.iq>
2022-03-10 14:05:19 +0300

    send req

--- redoc-tryout.js
+++ redoc-tryout.js
@@ -1,32 +1,32 @@
-function sendRequest(host, path, method, body, headers, https) {
-  param = {
-    url: (https ? "https://" : "http://") + host + path,
-    type: method
-  }
-  if(body) {
-    param.body = body;
-  }
-  if(headers) {
-    param.headers = headers;
-  }
+// function sendRequest( method, body, headers) {
+//   param = {
+//     url: "http://172.18.211.201:5000/api/middleware-try-out",
+//     type: method
+//   }
+//   if(body) {
+//     param.body = body;
+//   }
+//   if(headers) {
+//     param.headers = headers;
+//   }
 
-  $target = $('#target')
-  $target.empty()
-  $('<img>').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Loading_icon_cropped.gif/40px-Loading_icon_cropped.gif').attr('width', '20').attr('height', '20').appendTo($target)
+//   $target = $('#target')
+//   $target.empty()
+//   $('<img>').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Loading_icon_cropped.gif/40px-Loading_icon_cropped.gif').attr('width', '20').attr('height', '20').appendTo($target)
 
-  $.ajax(param).done(function(data) {
-    $target.empty()
-    console.log("done:", data)
-    $('<span><b>Done</b></span>').appendTo($target)
-    $('<br>').appendTo($target)
-    $('<textarea>').attr('style', 'width:98%').attr('cols', '55').text(JSON.stringify(data)).appendTo($target)
-  }).fail((data) => {
-    $target.empty()
-    console.log("failed:", data)
-    $('<span><b>Failed</b></span>').appendTo($target)
-    $('<span>').text(data.statusText).appendTo($target)
-  })
-}
+//   $.ajax(param).done(function(data) {
+//     $target.empty()
+//     console.log("done:", data)
+//     $('<span><b>Done</b></span>').appendTo($target)
+//     $('<br>').appendTo($target)
+//     $('<textarea>').attr('style', 'width:98%').attr('cols', '55').text(JSON.stringify(data)).appendTo($target)
+//   }).fail((data) => {
+//     $target.empty()
+//     console.log("failed:", data)
+//     $('<span><b>Failed</b></span>').appendTo($target)
+//     $('<span>').text(data.statusText).appendTo($target)
+//   })
+// }
 
 function printRequestForm(swaggerJson, path, method) {
   $('#target').empty()
@@ -60,56 +60,56 @@ function printRequestForm(swaggerJson, path, method) {
   $('#form').show();
 }
 
-function collectDataAndSendRequest() {
-  queryParams = $('input[in=query]').toArray().map(input => [input.name, input.value])
-  pathParams = $('input[in=path]').toArray().map(input => [input.name, input.value])
-  headerParams = $('input[in=header]').toArray().map(input => [input.name, input.value])
-  body = $('input[in=body]').toArray().map(input => input.value).pop()
-  uri = $('input[name=uri]').toArray().map(input => input.value).pop()
-  method = $('input[name=method]').toArray().map(input => input.value).pop()
-  formDataParams = $('input[in=formData]').toArray().map(input => [input.name, input.value])
+// function collectDataAndSendRequest() {
+//   queryParams = $('input[in=query]').toArray().map(input => [input.name, input.value])
+//   pathParams = $('input[in=path]').toArray().map(input => [input.name, input.value])
+//   headerParams = $('input[in=header]').toArray().map(input => [input.name, input.value])
+//   body = $('input[in=body]').toArray().map(input => input.value).pop()
+//   uri = $('input[name=uri]').toArray().map(input => input.value).pop()
+//   method = $('input[name=method]').toArray().map(input => input.value).pop()
+//   formDataParams = $('input[in=formData]').toArray().map(input => [input.name, input.value])
 
-  pathParams.map(param => {
-    uri = uri.replace("{" + param[0] + "}", param[1])
-  })
-  if(queryParams.length > 0) {
-    uri += "?" + queryParams.filter(p => p[1] && p[1] != '')
-        .map(p => encodeURI(p[0])+ "=" + encodeURI(p[1]))
-        .join("&")
-  }
+//   pathParams.map(param => {
+//     uri = uri.replace("{" + param[0] + "}", param[1])
+//   })
+//   if(queryParams.length > 0) {
+//     uri += "?" + queryParams.filter(p => p[1] && p[1] != '')
+//         .map(p => encodeURI(p[0])+ "=" + encodeURI(p[1]))
+//         .join("&")
+//   }
 
-  if(!body && formDataParams.length > 0) {
-    body = formDataParams.filter(p => p[1] && p[1] != '')
-        .map(p => encodeURI(p[0])+ "=" + encodeURI(p[1]))
-        .join("&")
-  }
+//   if(!body && formDataParams.length > 0) {
+//     body = formDataParams.filter(p => p[1] && p[1] != '')
+//         .map(p => encodeURI(p[0])+ "=" + encodeURI(p[1]))
+//         .join("&")
+//   }
 
-  var headers = headerParams.reduce((m,xy) => {m[xy[0]]=xy[1] ; return m;}, {})
-  var https = $('#https').prop('checked') ? true : undefined;
-  sendRequest(swaggerJson.host + (swaggerJson.basePath ? swaggerJson.basePath : ""), uri, method, body, Object.keys(headers).length != 0 ? headers : undefined, https)
-}
+//   var headers = headerParams.reduce((m,xy) => {m[xy[0]]=xy[1] ; return m;}, {})
+//   //var https = $('#https').prop('checked') ? true : undefined;
+//   sendRequest( method, body, Object.keys(headers).length != 0 ? headers : undefined)
+// }
 
 
-$('#sendButton').click(() => {
-  collectDataAndSendRequest();
-})
+// $('#sendButton').click(() => {
+//   collectDataAndSendRequest();
+// })
 
-var swaggerJson = null;
-swaggerUrl = $('redoc').attr('spec-url')
-$.ajax({
-  url: swaggerUrl
-}).done(data => {
-  swaggerJson = typeof data == 'string' ? jsyaml.load(data): data
-  if(!swaggerJson.host) {
-    swaggerJson.host = defaultHost
-  }
-})
+// var swaggerJson = null;
+// swaggerUrl = $('redoc').attr('spec-url')
+// $.ajax({
+//   url: swaggerUrl
+// }).done(data => {
+//   swaggerJson = typeof data == 'string' ? jsyaml.load(data): data
+//   if(!swaggerJson.host) {
+//     swaggerJson.host = defaultHost
+//   }
+// })
 
 $(document).ready(() => {
 
 
   setTimeout(() => {
-    $('.http-verb').toArray().map(c =>$(c).parent().append($('<p>')).append($("<div style='border-radius: 5px; background: white; padding: 5px; width:50px; color: black;' class='try-out'>try-out</div>")))
+    $('.http-verb').toArray().map(c => $(c).parent().append($('<p>')).append($("<div style='border-radius: 5px; background: white; padding: 5px; width:50px; color: black;' class='try-out'>try-out</div>")))
     $('.try-out').click((event) => {
       verbNode = $($(event.target).parent().children()[0])
       var method = verbNode.text().trim();
@@ -121,4 +121,5 @@ $(document).ready(() => {
     });
 
   }, typeof redocLoadingWaitTime == 'undefined' ? 8000 : redocLoadingWaitTime);
+
 })
